bb6ec2cc49abc28de82240e0b244097b3e858591
cl4pper <cl4pper@gmail.com>
2020-12-27 19:33:59 -0300

    test: add tests for Button lib

---
+++ src/lib/Button/Button.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { Button } from './Button';
+
+const label = 'Button label';
+
+describe('[lib] Button:', () => {
+	it('should be rendered', () => {
+		const { container } = render(<Button />);
+
+		expect(container.firstChild.className).toBe('Button');
+	});
+
+	it('should has a label and NO loading icon', () => {
+		const { queryAllByText, queryAllByTestId } = render(<Button label={label} />);
+
+		expect(queryAllByText(label).length).toBe(1);
+		expect(queryAllByTestId('button-loading-icon').length).toBe(0);
+	});
+
+	it('should apply secondary style when secondary prop is set', () => {
+		const { container } = render(<Button secondary />);
+
+		expect(container.firstChild.classList.contains('Button--secondary')).toBeTruthy();
+	});
+
+	it('should show loader icon and hide label while loading', () => {
+		const { queryAllByTestId, queryAllByText } = render(<Button loading label={label} />);
+
+		expect(queryAllByText(label).length).toBe(0);
+		expect(queryAllByTestId('button-loading-icon').length).toBe(1);
+	});
+});
