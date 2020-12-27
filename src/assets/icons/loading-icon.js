064d5f4188b1a038fa39dd52c542f886c0c873f7
cl4pper <cl4pper@gmail.com>
2020-12-27 19:34:56 -0300

    feat: add testid into loading-icon
    * to easy tests of components using svg add data-testid into icons

--- src/assets/icons/loading-icon.js
+++ src/assets/icons/loading-icon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LoadingIcon = ({ width, height, viewBox, className }) => {
+const LoadingIcon = ({ width, height, viewBox, className, dataTestId }) => {
 	const props = {
 		width: width || '20px',
 		height: height || '20px',
@@ -16,9 +16,10 @@ const LoadingIcon = ({ width, height, viewBox, className }) => {
 			focusable="false"
 			data-prefix="fas"
 			data-icon="spinner"
+			data-testid={dataTestId}
 			role="img"
 			xmlns="http://www.w3.org/2000/svg"
-			class={className}>
+			className={className}>
 				<path fill="currentColor" d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z" class=""></path>
 		</svg>
 	);
