2a62d1b3916b33b9e83c322467742e1ec2a51c79
cl4pper <cl4pper@gmail.com>
2020-12-28 16:35:22 -0300

    feat: create lib IconsHandler
    * call IconsHandler instead of any svg icon directly

---
+++ src/lib/IconsHandler/IconsHandler.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// ICONS
+import { LoadingIcon, CloseIcon } from '@icons';
+
+const IconsHandler = ({ icon, small, large, className, dataTestId }) => {
+	// set icon size
+	const setSize = () => {
+		if (small) {
+			return {
+				height: '16px',
+				width: '16px'
+			};
+		}
+
+		if (large) {
+			return {
+				height: '32px',
+				width: '32px'
+			};
+		}
+
+		return {
+			height: '20px',
+			width: '20px'
+		};
+	};
+
+	const svgProps = {
+		...setSize(),
+		className,
+		dataTestId
+	};
+
+	// select respective icon svg icon
+	switch(icon) {
+		case 'loader': return <LoadingIcon {...svgProps} />;
+		case 'close': return <CloseIcon {...svgProps} />;
+		default: return <LoadingIcon {...svgProps} />;
+	}
+};
+
+export { IconsHandler };
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,5 @@
 export * from './Button/Button';
+export * from './IconsHandler/IconsHandler';
 export * from './Text/Text';
 export * from './TextInput/TextInput';
 export * from './Title/Title';
